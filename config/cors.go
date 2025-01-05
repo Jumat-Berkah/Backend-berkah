ba11e06bd70e6ae7ce7ca41be267395277d1b887
rrq-dev <raihanhermawan85@gmail.com>
2025-01-05 16:49:26 +0700

    update cors

--- config/cors.go
+++ config/cors.go
@@ -9,14 +9,14 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
 		w.Header().Set("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE")
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:8080")
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.WriteHeader(http.StatusNoContent)
 		return true
 	}
 	// Set CORS headers for the main request.
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Origin", "127.0.0.1:8080")
+	w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
 	w.Header().Set("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE")
 	return false
 }
