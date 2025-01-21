b146b9e70ac7bffcb0bf2f2708de5ac192108745
rrq-dev <raihanhermawan85@gmail.com>
2025-01-21 19:42:30 +0700

    update validasi input

--- controller/auth.go
+++ controller/auth.go
@@ -38,15 +38,16 @@ func Register(w http.ResponseWriter, r *http.Request) {
         return        
     }        
   
-    // Validate input          
-	if requestData.Password != requestData.ConfirmPassword {          
-    w.Header().Set("Content-Type", "application/json")          
-    w.WriteHeader(http.StatusBadRequest)          
-    json.NewEncoder(w).Encode(map[string]string{          
-        "message": "Passwords do not match.",          
-    })          
-    return          
-}          
+	//validate input
+	if requestData.Password != requestData.ConfirmPassword {  
+		w.Header().Set("Content-Type", "application/json")  
+		w.WriteHeader(http.StatusBadRequest)  
+		json.NewEncoder(w).Encode(map[string]string{  
+			"message": "Passwords do not match.",  
+		})  
+		return  
+	}  
+	 
    
   
     if requestData.Email == "" || requestData.Username == "" || requestData.Password == "" {        
