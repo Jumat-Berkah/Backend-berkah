7d5d6fca6115335587ea15966a52501ed6c12b3a
rrq-dev <raihanhermawan85@gmail.com>
2025-01-21 16:54:24 +0700

    update cors

--- config/cors.go
+++ config/cors.go
@@ -5,7 +5,7 @@ import "net/http"
 // SetAccessControlHeaders handles CORS for preflight and main requests
 func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 	// Set CORS headers for all requests
-	w.Header().Set("Access-Control-Allow-Origin", "https://rrq-dev.github.io") 
+	w.Header().Set("Access-Control-Allow-Origin", "https://rrq-dev.github.io/jumatberkah.github.io/") 
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Login, X-Requested-With")
