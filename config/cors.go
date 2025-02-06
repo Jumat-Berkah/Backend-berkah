b3fde38908c2cf2efef6ffc1bf719fff9f69e19e
rrq-dev <raihanhermawan85@gmail.com>
2025-02-06 12:05:24 +0700

    tidak menggunakan origins

--- config/cors.go
+++ config/cors.go
@@ -2,25 +2,6 @@ package config
 
 import "net/http"
 
-var Origins = []string{
-    "https://jumatberkah.vercel.app",
-	
-}
-
-var Headers = []string{
-	"Origin",
-	"Content-Type",
-	"Accept",
-	"Authorization",
-	"Access-Control-Request-Headers",
-	"Token",
-	"Login",
-	"Access-Control-Allow-Origin",
-	"Bearer",
-	"X-Requested-With",
-} 
-
-
 func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {  
     // Set CORS headers
     w.Header().Set("Access-Control-Allow-Origin", "https://jumatberkah.vercel.app")
